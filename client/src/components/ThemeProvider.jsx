5c77e11a61d7ec92645e813217972a1d94876501
project_Html <khoanguyenduc99@gmail.com>
2024-08-27 19:32:47 +0700

    completed darkmode functionality using redux

---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+export default function ThemeProvider({children}) {
+    const {theme} = useSelector(state => state.theme);
+
+  return (
+    <div className={theme}>
+        <div className="bg-white
+                         text-gray-700 
+                         dark:text-gray-200 
+                         dark:bg-[rgb(15,25,54)]
+                         min-h-screen"
+        >
+            {children}
+
+        </div>
+    </div>
+  )
+}
