0e7edadb8e0ec3e5f67577723c0bec3c7913657f
project_Html <khoanguyenduc99@gmail.com>
2024-08-31 20:46:55 +0700

    add edit function in a comment to handle update a comment

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import moment from 'moment';
 import {FaThumbsUp} from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({comment, onLike}) {
+export default function Comment({comment, onLike, onEdit}) {
     const [user, setUser] = useState({});
-    const {currentUser} = useSelector(state => state.user)
+    const [isEditting, setIsEditting] = useState(false);
+    const [editedContent, setEditedContent] = useState(comment.content);
+    const {currentUser} = useSelector(state => state.user);
 
     useEffect(() => {
         const getUser = async () => {
@@ -20,7 +23,34 @@ export default function Comment({comment, onLike}) {
             }
         };
         getUser();
-    }, [comment])
+    }, [comment]);
+    
+
+    const handleEdit = () => {
+        setIsEditting(true);
+        setEditedContent(comment.content);
+
+    }
+    const handleSave = async() => {
+        try {
+            const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    content: editedContent
+                })
+            });
+            if (res.ok){
+                setIsEditting(false);
+                onEdit(comment, editedContent);
+            }
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
     
     return (
     <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
@@ -34,14 +64,50 @@ export default function Comment({comment, onLike}) {
                     {moment(comment.createdAt).fromNow()}
                 </span>
             </div>
-            <p className='text-gray-500 dark:text-green-300 mb-2'>{comment.content}</p>
-            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
-                <button className={`text-gray-400 hover:text-sky-300 ${currentUser && comment.likes.includes(currentUser._id) && '!text-sky-300'} `} 
-                        type='button' onClick={() => onLike(comment._id)}>
-                    <FaThumbsUp className='text sm '/>
-                </button>
-                <p className='text-gray-400'>{comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "Like" : "Likes") }</p>
-            </div>
+            {
+                isEditting ? (
+                    <>
+                        <Textarea className='mb-2' 
+                                value={editedContent}
+                                onChange={(event) => setEditedContent(event.target.value)}
+                        />
+                        <div className="flex justify-end gap-2 text-xs">
+                            <Button type='button' size='sm' gradientDuoTone='purpleToPink' onClick={handleSave}>
+                                Save
+                            </Button>
+                            
+                            <Button type='button' size='sm' 
+                                    gradientDuoTone='purpleToBlue' 
+                                    outline
+                                    onClick={() => setIsEditting(false)}
+                            >
+                                Cancel
+                            </Button>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <p className='text-gray-500 dark:text-green-300 mb-2'>{comment.content}</p>
+                        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+                            <button className={`text-gray-400 hover:text-sky-300 ${currentUser && comment.likes.includes(currentUser._id) && '!text-sky-300'} `} 
+                                    type='button' onClick={() => onLike(comment._id)}>
+                                <FaThumbsUp className='text sm '/>
+                            </button>
+                            <p className='text-gray-400'>{comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "Like" : "Likes") }</p>
+                            {
+                                currentUser && (currentUser._id === comment.userId || currentUser.isAdmin ) && (
+                                    <button type='button' 
+                                        onClick={handleEdit}
+                                        className='text-gray-400 hover:text-sky-300'
+                                    >
+                                        Edit
+                                    </button>
+                                )   
+                            }
+                        </div>
+                    </>
+                )
+            }
         </div>
     </div>
   )
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -81,7 +81,15 @@ export default function CommentSection({postId}) {
             }
         }
         getComments();
-    }, [postId])
+    }, [postId]);
+
+    const handleEdit = async (comment, editedContent) => {
+        setComments(
+            comments.map((c) => 
+                c._id === comment._id ? {...c, content: editedContent} : c
+            )
+        )
+    };
     return (
     <div className='max-w-2xl mx-auto w-full p-3 '>
         {currentUser ? (
@@ -137,6 +145,7 @@ export default function CommentSection({postId}) {
                                 <Comment key={comment._id}
                                     comment={comment}
                                     onLike={handleLike}
+                                    onEdit={handleEdit}
                                 />
                             ))
                         }
